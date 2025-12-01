30c82be47214285a256d8cb109e5dfff6015234d
Randy Treit <randy.treit@scvsar.org>
2025-12-01 11:54:13 -0800

    Day 1 part 1

---
+++ 2025/day1/part1.py
@@ -0,0 +1,36 @@
+def generate_line_tuple(line) -> tuple:
+    try:
+        line_tuple: tuple = (line[0], int(line[1:]))
+        return line_tuple
+    except Exception as e:
+        print(f"Hit an exception: {e}")
+        return {}
+
+def turn_dial(dial: list, current_index: int, step:int, direction: str) -> int:
+    min_value = min(dial)
+    max_value = max(dial)
+    for _ in range(step):
+        if direction == "L":
+            if current_index == min_value:
+                current_index = max_value               
+            else:
+                current_index = current_index - 1
+        if direction == "R":
+            if current_index == max_value:
+                current_index = min_value                
+            else:
+                current_index = current_index + 1
+    return current_index
+
+dial = [x for x in range(100)]
+current_index = 50
+with open("input.txt") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+line_tuples = [generate_line_tuple(line) for line in lines]
+zero_count = 0
+for t in line_tuples:
+    current_index = turn_dial(dial, current_index, t[1], t[0])
+    if current_index == 0:
+        zero_count += 1
+print(zero_count)
