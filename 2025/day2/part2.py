d0b20b812623f3677c2002b89d8d5a7906c757d5
Randy Treit <randy.treit@scvsar.org>
2025-12-02 14:40:37 -0800

    Day 2 part 2 in progress

---
+++ 2025/day2/part2.py
@@ -0,0 +1,40 @@
+with open("input.txt") as f:
+    input: list = f.readline().split(",")
+
+def calculate_ngrams(ngram: int, text: str) -> list:
+    print(text)
+    ngrams = []
+    for i in range(len(text)):
+        gram = text[i:i+ngram]
+        ngrams.append(gram)
+    return ngrams
+
+
+def has_repeating_pattern(number: int) -> bool:
+    result = False
+    # find all possible ngrams
+    ngrams: list = []
+    grams = len(str(number)) // 2
+    for j in range(1,grams+1):
+        ngrams.append(j)    
+    print(ngrams)
+
+
+    return result
+
+def find_invalid(input: str) -> list:
+    input = input.split("-")
+    candidates: list = [candidate for candidate in range(int(input[0]), int(input[1]) + 1)]
+    invalid_values = [candidate for candidate in candidates if has_repeating_pattern(candidate)]
+    return invalid_values
+
+results = []
+for i in enumerate(input):
+    results = results + find_invalid(i[1])
+    
+print(results)
+print(f"Sum of invalid IDs: {sum(results)}")
+
+print(calculate_ngrams(4, "1231231"))
+
+
